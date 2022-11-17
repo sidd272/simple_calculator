c7c75587549f989444b3d994b89c045d4cc4bf78
Siddharth Kelkar <siddharthkelkar.211it067@nitk.edu.in>
2022-11-17 16:50:54 +0530

    adding delete and favicon

--- script.js
+++ script.js
@@ -9,7 +9,12 @@ Array.from(buttons).forEach((button) => {
         } else if (e.target.innerHTML == "C") {
             string = "";
             document.querySelector('input').value = string
-        } else {
+        }
+        else if (e.target.innerHTML == "Del") {
+            string = string.slice(0,string.length-1);
+            document.querySelector('input').value = string
+        }
+             else {
             console.log(e.target);
             string = string + e.target.innerHTML;
             document.querySelector('input').value = string
